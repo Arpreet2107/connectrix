e92e2da67af65cae0e6783134bc1bb481e9db6b9
Arpreet <arpreet4114@gmail.comm>
2025-06-19 19:48:44 +0530

    ⚙️ Added application.yml with PostgreSQL, Keycloak, and upload path configuration

---
+++ connectrix/src/main/java/com/connectrix_back/connectrix/ConnectrixApiApplication.java
@@ -0,0 +1,13 @@
+package com.connectrix_back.connectrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectrixApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectrixApiApplication.class, args);
+	}
+
+}
